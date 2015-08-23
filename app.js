639433a1910c7c02961f31b0b76f07295d983dde
iansayre <me@iansayre.com>
2015-08-22 23:15:39 -0700

    can print material list to console

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ var chess = function( currentPos, color ){
 					blackPawn: 7, blackkNight: 8, blackBishop: 9, blackRook: 10, blackQueen: 11, blackKing: 12 },
 		colors = { white: 0, black: 1, both: 2 },
 		defaultPosition = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR',
+		fileChars = 'abcdefgh', // characters to represent the file
 		files = { fileA: 0, fileB: 1, fileC: 2, fileD: 3, fileE: 4, fileF: 5, fileG: 6, fileH: 7, noFile: 8 },
-		ranks = { rank1: 0, rank2: 1, rank3: 2, rank4: 3, rank5: 4, rank6: 5, rank7: 6, rank8: 7, noRank: 8 },
 		keySquares = { a1: 21, b1: 22, c1: 23, d1: 24, e1: 25, f1: 26, g1: 27, h1: 28,
 					a8: 91, b8: 92, c8: 93, d8: 94, e8: 95, f8: 96, g8: 97, h8: 98,
 					noSquare: 99, offBoard: 100},
 		maxDepth = 64, // max search depth
 		maxMoves = 256, // max number of moves
+		pieceChars = '.PNBRQKpnbrqk', //characters to represent the pieces
+		rankChars = '12345678', //characters to represent the ranks
+		ranks = { rank1: 0, rank2: 1, rank3: 2, rank4: 3, rank5: 4, rank6: 5, rank7: 6, rank8: 7, noRank: 8 },
 		sideKey;
 
 	// set up arrays
@@ -30,7 +33,7 @@ var chess = function( currentPos, color ){
 	// set up the game board
 	var gameBoard = {
 		hisPly: 0,
-		material: new Array( 2 ),
+		// material: new Array( 2 ),
 		moveList: new Array( maxDepth * maxMoves ),
 		moveListStart: new Array( maxDepth ),
 		pieces: new Array( boardSquares ),
@@ -52,17 +55,23 @@ var chess = function( currentPos, color ){
 		return( ( 21 + file ) + ( rank * 10 ) );
 	};
 
-	// generate random numbers
-	var randoNums = function() {
-		return ( Math.floor( ( Math.random() * 255 ) + 1 ) << 23 ) | ( Math.floor( ( Math.random() * 255 ) + 1 ) << 16 ) | ( Math.floor( ( Math.random() * 255 ) + 1 ) << 8 ) | Math.floor( ( Math.random() * 255 ) + 1 );
-	};
+	var generatePositionKey = function() {
+		var finalKey = 0,
+			piece = chessPieces.empty,
+			sq = 0;
 
-	var squareSixtyFour = function( squareOneTwenty ) {
-		return oneTwentyTo64[ squareOneTwenty ];
-	};
+		for( sq; sq < boardSquares; sq++ ) {
+			piece = gameBoard.pieces[sq];
+			if( piece != chessPieces.empty && piece != keySquares.offBoard ) {
+				finalKey ^= pieceKeys[ ( piece * 120 ) + sq ];
+			}
+		}
 
-	var squareOneTwenty = function( squareSixtyFour ) {
-		return sixtyFourTo120[ squareSixtyFour ];
+		if( gameBoard.side === colors.white ) {
+			finalKey ^= sideKey;
+		}
+
+		return finalKey;
 	};
 
 	var initFileRankBoard = function() {
@@ -83,97 +92,43 @@ var chess = function( currentPos, color ){
 			}
 		}
 	};
-		// set up the game board
 
-	var pieceIndex = function( piece, pieceNum ) {
-		return ( piece * 10 + pieceNum );
+	var initPieceKeys = function(){
+		for ( var i = 0; i < pieceKeys.length; i++ ) {
+			pieceKeys[i] = randoNums();
+		}
+
+		sideKey = randoNums();
 	};
 
-	var printBoard = function() {
-		var boardFile,
-			boardPiece,
-			boardRank,
-			boardSquare,
-			fileChars = 'abcdefgh',
-			pieceChars = '.PNBRQKpnbrqk';// letters to represent the characters;
+	var init120squaresTo64 = function() {
+		var file = files.fileA,
+			rank = ranks.rank1,
+			square = keySquares.a1,
+			sq64 = 0;
 
-		console.log('Game Board: ' );
-
-		for( boardRank = ranks.rank8; boardRank >= ranks.rank1; boardRank-- ){
-			var line = boardRank + ' ';
-			for( boardFile = files.fileA; boardFile <= files.fileH; boardFile++ ) {
-				boardSquare = fileRankSquare( boardFile, boardRank );
-				boardPiece = gameBoard.pieces[boardSquare];
-				line += ' ' + pieceChars[boardPiece] + ' ';
-			}
-			console.log( line );
+		for ( var i = 0; i < boardSquares; i++ ) {
+			// something off the 64 square board
+			oneTwentyTo64[i] = 65;
 		}
-		var newLine = ' ';
-		for( boardFile = files.fileA; boardFile <= files.fileH; boardFile++ ) {
-			newLine += ' ' + fileChars[boardFile] + ' ';
+
+		for ( var j = 0; j < 64; j++ ) {
+			sixtyFourTo120[j] = 120;
 		}
-		console.log( newLine );
-	};
 
-	var generatePositionKey = function() {
-		var finalKey = 0,
-			piece = chessPieces.empty,
-			sq = 0;
-
-		for( sq; sq < boardSquares; sq++ ) {
-			piece = gameBoard.pieces[sq];
-			if( piece != chessPieces.empty && piece != keySquares.offBoard ) {
-				finalKey ^= pieceKeys[ ( piece * 120 ) + sq ];
+		for( rank = ranks.rank1; rank <= ranks.rank8; rank++ ) {
+			for( file = files.fileA; file <= files.fileH; file++ ) {
+				square = fileRankSquare( file, rank );
+				sixtyFourTo120[sq64] = square;
+				oneTwentyTo64[square] = sq64;
+				sq64++;
 			}
 		}
-
-		if( gameBoard.side === colors.white ) {
-			finalKey ^= sideKey;
-		}
-
-		return finalKey;
 	};
 
-	var resetBoard = function() {
-		// reset outside squares to offboard
-		for( var i = 0; i < boardSquares; i++ ) {
-			gameBoard.pieces[i] = keySquares.offBoard;
-		}
-
-		console.log( 'OFFBOARD RESET' );
-
-		// reset board squares to empty
-		for( var s = 0; s < 64; s++ ) {
-			gameBoard.pieces[ squareOneTwenty(s) ] = chessPieces.empty;
-		}
-		console.log( 'PIECES RESET' );
-
-		// reset chess pieces to empty
-		for( var p = 0; p < 14 * 120; p++ ) {
-			gameBoard.pieceList[p] = chessPieces.empty;
-		}
-
-		console.log( 'PIECELIST RESET' );
-
-		// reset the piece material
-		for( var m = 0; m < 2; m++ ) {
-			gameBoard.material[m] = 0;
-		}
-
-		console.log( 'MATERIAL RESET' );
-
-		// reset number of pieces
-		for( var n = 0; n < 13; n++ ) {
-			gameBoard.pieceNum[n] = 0;
-		}
-
-		console.log( 'PIECENUM RESET');
-
-		gameBoard.side = colors.both;
-		gameBoard.ply = 0;
-		gameBoard.hisPly = 0;
-		gameBoard.moveListStart[gameBoard.ply] = 0;
-		gameBoard.posKey = 0;
+	var parseColor = function( color ) {
+		gameBoard.side = color === 'white' ? colors.white : colors.black;
+		console.log('COLOR: ' + gameBoard.side );
 	};
 
 	var parsePosFEN = function( currentPos ) {
@@ -264,43 +219,125 @@ var chess = function( currentPos, color ){
 			}
 			fenCount++;
 		}
+
+		updateMaterialList();
 	};
 
-	var parseColor = function( color ) {
-		gameBoard.side = color === 'white' ? colors.white : colors.black;
-		console.log('COLOR: ' + gameBoard.side );
+	var pieceIndex = function( piece, pieceNum ) {
+		return ( piece * 10 + pieceNum );
 	};
 
-	var initPieceKeys = function(){
-		for ( var i = 0; i < pieceKeys.length; i++ ) {
-			pieceKeys[i] = randoNums();
-		}
+	var printBoard = function() {
+		var boardFile,
+			boardPiece,
+			boardRank,
+			boardSquare;// letters to represent the characters;
 
-		sideKey = randoNums();
+		console.log('Game Board: ' );
+
+		for( boardRank = ranks.rank8; boardRank >= ranks.rank1; boardRank-- ){
+			var line = boardRank + ' ';
+			for( boardFile = files.fileA; boardFile <= files.fileH; boardFile++ ) {
+				boardSquare = fileRankSquare( boardFile, boardRank );
+				boardPiece = gameBoard.pieces[boardSquare];
+				line += ' ' + pieceChars[boardPiece] + ' ';
+			}
+			console.log( line );
+		}
+		var newLine = ' ';
+		for( boardFile = files.fileA; boardFile <= files.fileH; boardFile++ ) {
+			newLine += ' ' + fileChars[boardFile] + ' ';
+		}
+		console.log( newLine );
+
+		printPieces();
 	};
 
+	var printPieces =  function() {
+		var piece,
+			pieceNum;
+		for ( piece = chessPieces.whitePawn; piece <= chessPieces.blackKing; piece++ ) {
+			for ( pieceNum = 0; pieceNum < gameBoard.pieceNum[piece]; pieceNum++ ) {
+				console.log( pieceChars[piece] + ' is on ' + printSquare( gameBoard.pieceList[pieceIndex( piece, pieceNum )] ) )
+			}
+		}
+	};
 
-	var init120squaresTo64 = function() {
-		var file = files.fileA,
-			rank = ranks.rank1,
-			square = keySquares.a1,
-			sq64 = 0;
+	var printSquare = function(sq) {
+		return( fileChars[fileBoard[sq]] + ':' + rankChars[rankBoard[sq]] );
+	};
 
-		for ( var i = 0; i < boardSquares; i++ ) {
-			// something off the 64 square board
-			oneTwentyTo64[i] = 65;
+	// generate random numbers
+	var randoNums = function() {
+		return ( Math.floor( ( Math.random() * 255 ) + 1 ) << 23 ) | ( Math.floor( ( Math.random() * 255 ) + 1 ) << 16 ) | ( Math.floor( ( Math.random() * 255 ) + 1 ) << 8 ) | Math.floor( ( Math.random() * 255 ) + 1 );
+	};
+
+	var resetBoard = function() {
+		// reset outside squares to offboard
+		for( var i = 0; i < boardSquares; i++ ) {
+			gameBoard.pieces[i] = keySquares.offBoard;
 		}
 
-		for ( var j = 0; j < 64; j++ ) {
-			sixtyFourTo120[j] = 120;
+		console.log( 'OFFBOARD RESET' );
+
+		// reset board squares to empty
+		for( var s = 0; s < 64; s++ ) {
+			gameBoard.pieces[ squareOneTwenty(s) ] = chessPieces.empty;
+		}
+		console.log( 'PIECES RESET' );
+
+		gameBoard.side = colors.both;
+		gameBoard.ply = 0;
+		gameBoard.hisPly = 0;
+		gameBoard.moveListStart[gameBoard.ply] = 0;
+		gameBoard.posKey = 0;
+	};
+
+	var squareSixtyFour = function( squareOneTwenty ) {
+		return oneTwentyTo64[ squareOneTwenty ];
+	};
+
+	var squareOneTwenty = function( squareSixtyFour ) {
+		return sixtyFourTo120[ squareSixtyFour ];
+	};
+
+	var updateMaterialList = function() {
+		var piece,
+			pieceColor,
+			square;
+
+		// reset chess pieces to empty
+		for( var p = 0; p < 14 * 120; p++ ) {
+			gameBoard.pieceList[p] = chessPieces.empty;
 		}
 
-		for( rank = ranks.rank1; rank <= ranks.rank8; rank++ ) {
-			for( file = files.fileA; file <= files.fileH; file++ ) {
-				square = fileRankSquare( file, rank );
-				sixtyFourTo120[sq64] = square;
-				oneTwentyTo64[square] = sq64;
-				sq64++;
+		console.log( 'PIECELIST RESET' );
+
+		// reset the piece material
+		// for( var m = 0; m < 2; m++ ) {
+			// gameBoard.material[m] = 0;
+		// }
+
+		// console.log( 'MATERIAL RESET' );
+
+		// reset number of pieces
+		for( var n = 0; n < 13; n++ ) {
+			gameBoard.pieceNum[n] = 0;
+		}
+
+		console.log( 'PIECENUM RESET');
+
+		for( var i = 0; i < 64; i++ ) {
+			square = squareOneTwenty(i);
+			piece = gameBoard.pieces[square];
+
+			if( piece != chessPieces.empty ) {
+				console.log('MATERIAL!! piece ' + piece + ' is on square ' + square);
+				pieceColor = colors[ piece ];
+
+				// gameBoard.mater
+				gameBoard.pieceList[pieceIndex( piece, gameBoard.pieceNum[piece] )] = square;
+				gameBoard.pieceNum[piece]++;
 			}
 		}
 	};
@@ -311,6 +348,8 @@ var chess = function( currentPos, color ){
 	parsePosFEN( currentPos );
 	parseColor( color );
 	printBoard();
+
+
 };
 
-var chessGame = new chess( 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1', 'black');
+var chessGame = new chess( 'r2rb1k1/pp1q1p1p/2n1p1p1/2bp4/5P2/PP1BPR1Q/1BPN2PP/R5K1', 'black');
